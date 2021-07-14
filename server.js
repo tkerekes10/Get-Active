1eb38b516dacee579306eab26af8eabe5a28c3c2
Tom Kerekes <tkerekes10@gmail.com>
2021-07-13 20:00:54 -0400

    Added .env in order to allow the app to work in heroku

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/27017", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
+
+console.log(process.env);
 
 // routes
 
