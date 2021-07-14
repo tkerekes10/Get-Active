a06a78c9bc89982869e768914d228929d75bd7b0
Tom Kerekes <tkerekes10@gmail.com>
2021-07-13 20:08:47 -0400

    This should be the final commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/27017", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
